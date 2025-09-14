cbf061bd0c22ed38a60d575677698aa76858f747
Eran Sandler <eran@sandler.co.il>
2025-09-13 20:12:59 -0700

    test5

--- src/vulnerable-code.js
+++ src/vulnerable-code.js
@@ -1,5 +1,5 @@
 // WARNING: This code has security vulnerabilities for testing purposes
-// Trigger build 4
+// Trigger build 5
 
 function login(username, password) {
     // SQL injection vulnerability
